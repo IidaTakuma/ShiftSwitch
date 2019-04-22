976fef4e7ecc7c60611371c64cf6db5ebe69bfef
User Name <ta9maxxx@gmail.com>
2019-04-23 02:20:00 +0900

    休日のみをテンプレートに表示できるようにした

---
+++ ShiftSwitch/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from .views import *
+
+app_name = 'main'
+
+urlpatterns = [
+    path('',views.CalendarView.as_view(), name="calender"),
+]
--- ShiftSwitch/main/views.py
+++ ShiftSwitch/main/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
+from datetime import date, timedelta
 # Create your views here.
+
+class CalendarView(TemplateView):
+
+    template_name = "main/calendar.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["weekends"] = self.get_weekends
+        return context
+    
+    def get_weekends(self):
+        this_month = date.today()
+
+        end_day = (date(this_month.year, this_month.month+1, 1) - timedelta(days=1)).day
+
+        weekend = []
+        for day in range(1,int(end_day)+1):
+            d = date(this_month.year, this_month.month, day)
+            if d.weekday()==5:
+                weekend.append((day,"Sat"))
+            elif d.weekday()==6:
+                weekend.append((day,"Sun"))
+        
+        return weekend
