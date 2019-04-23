0129e5ac5569243bd28e5fd285d50e2889d07a52
User Name <ta9maxxx@gmail.com>
2019-04-23 17:29:35 +0900

    dailyページに日付情報を渡せるようにした。

--- ShiftSwitch/main/views/dailyView.py
+++ ShiftSwitch/main/views/dailyView.py
@@ -6,11 +6,17 @@ from django.views.generic import TemplateView
 class DailyView(TemplateView):
 
     template_name = "main/daily.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        month = self.request.GET.get('m')
+        day = self.request.GET.get('d')
+        week = self.request.GET.get('w')
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["weekends_thismonth"] = self.get_weekends_month(0)
-    #     context["weekends_nextmonth"] = self.get_weekends_month(1)
-    #     return context
+        context["month"] = month
+        context["day"] = day
+        context["week"] = week
+
+        return context
 
     
