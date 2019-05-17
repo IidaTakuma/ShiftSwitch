6a25f8d2ec28abe960ba41305c4e886d8f1a7efb
Ryo Hidaka <ryo.hi.19971219@gmail.com>
2019-05-18 07:59:17 +0900

    フッター固定用js追加

--- static/script.js
+++ static/script.js
@@ -1,4 +1,24 @@
+/* モーダル非表示(ここから) */
 $('#delModal').click(function () {
   $(this).parents('.alert').hide();
   return false;
 });
+/* モーダル非表示(ここまで) */
+
+/* フッター固定(ここから) */
+/*
+let page_size = {
+  w : $(window).width()
+ ,h : $(window).height()
+};
+let browser_size = {
+  w: document.body.clientWidth
+ ,h: document.body.clientHeight
+};
+if(page_size.h > browser_size.h) {
+  $('footer').css('position', 'fixed');
+  $('footer').css('bottom', '0px');
+  $('footer').css('width', '100%'); /* 必要なら */
+}
+*/
+/* フッター固定(ここまで) */
